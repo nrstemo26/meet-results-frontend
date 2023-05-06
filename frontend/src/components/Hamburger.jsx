b1b46b15ec2fe9ee6a3ea373af147a14efa4abe6
Nate Stemo <natestemo@hotmail.com>
2023-05-06 18:01:54 -0500

    separated mobilenav and hamburger

---
+++ frontend/src/components/Hamburger.jsx
@@ -0,0 +1,15 @@
+
+function Hamburger(){
+    return (
+        <div className="relative inline-block">
+            <a href="#">Home</a>
+            <a href="#">Meets</a>
+            <a href="#">Lifters</a>
+            <a href="#">Insights</a>
+            <a href="#">Login/Register</a>
+        </div>
+    )
+}
+
+
+export default Hamburger;
