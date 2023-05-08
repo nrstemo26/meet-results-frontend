c7a18de10858fa8154f0d5a44b355917b96a53b0
Nate Stemo <natestemo@hotmail.com>
2023-05-08 08:54:55 -0500

    can switch between dashboards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
 import './App.css'
+import {useState} from 'react'
 import Header from './components/Header'
 import MeetDashboard from './components/MeetDashboard/MeetDashboard'
+import LifterDashboard from './components/LifterDashboard/LifterDashboard'
 // import MobileNav from './components/MobileNav'
 
+
 function App() {
+  const [showMeet, setShowMeet] = useState(false)
   
+  const handleClick = () =>{
+    setShowMeet(curr=> !curr)
+  }
 
   return (
   //   <div className='bg-slate-500 h-full  '>
     <div className='h-full'>
       <Header isMobile={true}/>
-      <MeetDashboard></MeetDashboard>
+      <button className="border-4 border-orange-700" onClick={()=> handleClick()}>switch dashboards</button>
+      {showMeet ?<MeetDashboard /> :
+      <LifterDashboard/> }
+      {/* <LifterDashboard></LifterDashboard> */}
+      {/* <MeetDashboard></MeetDashboard> */}
       {/* <MobileNav/> */}
 
 
