fac3b7bb46b5c04acea0731c286250b6ad9d3099
Nate Stemo <natestemo@hotmail.com>
2023-05-08 12:55:20 -0500

    refactoring

--- frontend/src/components/LifterDashboard/AllTotals.jsx
+++ frontend/src/components/LifterDashboard/AllTotals.jsx
@@ -1,6 +1,6 @@
 function AllTotals(){
     return(
-        <div className="bg-slate-500 p-6 border-2 rounded-lg border-slate-500 flex flex-col overflow-hidden shadow-lg">
+        <div className="bg-secondary-500 p-6 rounded-lg flex flex-col overflow-hidden shadow-lg">
           <h1 className="text-lg font-bold">Meet Name and Date</h1>
           <div className="text-center flex flex-col gap-2">
               <a className="bg-white border-2 p-1 px-1 border-orange-700 rounded-lg">SN CJ TOTAL</a>
--- frontend/src/components/LifterDashboard/LifterDashboard.jsx
+++ frontend/src/components/LifterDashboard/LifterDashboard.jsx
@@ -9,7 +9,6 @@ import AllTotals from "./AllTotals";
 function LifterDashboard(){
     return (
         <div className="p-3 m-4 overflow-hidden shadow-lg">
-          
           <h1 className="text-center text-2xl font-bold">John Bars Lifter Dashboard</h1>
           <LifterChart/>
 
