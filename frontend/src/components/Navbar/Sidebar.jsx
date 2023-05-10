0c955966b877883e3fdc2b76ffa68f8c2c6960d9
Nate Stemo <natestemo@hotmail.com>
2023-05-10 09:11:11 -0500

    component class added

--- frontend/src/components/Navbar/Sidebar.jsx
+++ frontend/src/components/Navbar/Sidebar.jsx
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types'
 const Sidebar = ({toggleMenu}) =>{
     return(
         <div className="absolute z-40 top-0 left-0 h-screen w-48 m-0 flex flex-col gap-3 bg-white text-black shadow-lg">
-            <FiX className="pointer self-end mt-4 mr-4" size={30} color= "black" onClick={toggleMenu}/>
-            <i className="mx-4  border-b-2 border-primary-300">Home</i>
-            <i className="mx-4  border-b-2 border-primary-300 ">Meets</i>
-            <i className="mx-4  border-b-2 border-primary-300 ">Lifters</i>
-            <i className="mx-4 border-b-2 border-primary-300 ">Insights</i>
-            <i className="mx-4 border-b-2 border-primary-300 ">About Us</i>
-            <i className="mx-4 border-b-2 border-primary-300 ">Login/Sign Up</i>
+            <FiX className="cursor-pointer self-end mt-4 mr-4" size={30} color= "black" onClick={toggleMenu}/>
+            <a href="#" className="sidebar-element">Home</a>
+            <a href="#" className="sidebar-element ">Meets</a>
+            <a href="#" className="sidebar-element ">Lifters</a>
+            <a href="#" className="sidebar-element">Insights</a>
+            <a href="#" className="sidebar-element">About Us</a>
+            <a href="#" className="sidebar-element">Login/Sign Up</a>
         </div>
     )
 }
