d695f91e8d698a6d49417e17cef382d106ba3073
Nate Stemo <natestemo@hotmail.com>
2023-05-21 13:21:14 -0500

    takes in lifter data from back end and passes to components

--- frontend/src/components/Dashboards/LifterDashboard/LifterDashboard.jsx
+++ frontend/src/components/Dashboards/LifterDashboard/LifterDashboard.jsx
@@ -3,44 +3,63 @@ import LifterChart from "./LifterChart";
 import BestLifts from "./BestLits";
 import AllTotals from "./AllTotals";
 import {useState, useEffect} from 'react'
-import axios from 'axios'
-const baseUrl = 'http://192.168.1.247:5000/api/v1/athlete/Nathan%20Stemo'
+// import axios from 'axios'
+// const baseUrl = 'http://192.168.1.247:5000/api/v1/athlete/Nathan%20Stemo'
+import {useDispatch} from 'react-redux'
+import {getAthlete} from '../../../redux/lifterSlice'
 
 
-function LifterDashboard(){
-  console.log('sending a request')
-  const [athlete, setAthlete] = useState('')
+function LifterDashboard({userId='nathan'}){
+  const [id, setId] =useState('');
+  const [meetHistory, setMeetHistory] = useState(null)
+  const [stats, setStats] = useState(null)
+  const dispatch = useDispatch();
 
-  
   useEffect(()=>{
-    const fetchData = async () => {
-      try{
-        const res = await axios.get(baseUrl)
-        setAthlete(res.data)
-        console.log(res)
-        console.log('data returned')
-        console.log(res.data)
-      }catch(error){
-        console.error(error)
-      }
+    const getUserData = async()=>{
+      const {id, meet_history,stats} = (await dispatch(getAthlete())).payload
+      setId(id)
+      setMeetHistory(meet_history)
+      setStats(stats)
+      // console.log('stats',stats)
+      // console.log('history',meet_history)
     }
-    fetchData()
+    getUserData()
+  },[dispatch, userId])
+  
+  
+  
+  
+  // const [athlete, setAthlete] = useState('')
+  // useEffect(()=>{
+  //   const fetchData = async () => {
+  //     try{
+  //       const res = await axios.get(baseUrl)
+  //       setAthlete(res.data)
+  //       console.log(res)
+  //       console.log('data returned')
+  //       console.log(res.data)
+  //     }catch(error){
+  //       console.error(error)
+  //     }
+  //   }
+  //   fetchData()
 
       
-    },[])
+  //   },[])
     
     return (
       <>
           <div className='bg-secondary-500 p-5 rounded-xl'>
-            {/* <h1 className="text-center text-2xl font-bold">{athlete.id? athlete.id : 'loading'} Loading Dashboard</h1> */}
-            <h1 className="text-center text-2xl font-bold">John Bars Lifter Dashboard</h1>
+            <h1 className="text-center text-2xl font-bold">{id ? id : 'loading'} Dashboard</h1>
+            {/* <h1 className="text-center text-2xl font-bold">John Bars Lifter Dashboard</h1> */}
             <LifterChart/>
           </div>
 
         
         <div className="my-2 flex flex-wrap gap-2 justify-evenly">
-          <BestLifts />
-          <AllTotals />
+          <BestLifts stats={stats} />
+          <AllTotals meetHistory={meetHistory}/>
           <Insights />
         </div>
       </>
