96cafb09e56ae6af1ca00359114065fc65b54f74
Nate Stemo <natestemo@hotmail.com>
2023-05-22 12:47:53 -0500

    gets users from backend now

--- frontend/src/components/SearchBars/UserList.jsx
+++ frontend/src/components/SearchBars/UserList.jsx
@@ -1,16 +1,25 @@
 
 
 const UserList = ({ users }) => {
-
+  let lis = []
+  const renderUsers = (amount) => {
+    for( let i = 0; i<amount; i++){
+      if(users[i]){
+      let user = users[i];
+      lis.push(<div className='border-primary-500 p-2 border-2 m-2 hover:text-white hover:bg-primary-500 hover:cursor-pointer' key={user}>{user}</div> )
+      }
+    }
+    return lis;
+  }
+  
 
   return (
     <div>
       <h1>Search Results:</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user}>{user}</li>
-        ))}
-      </ul>
+      <div>
+        {renderUsers(20)}
+      
+      </div>
     </div>
   );
 }
