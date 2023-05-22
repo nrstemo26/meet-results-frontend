e8277d7b597aad8d3b659ccadefe2ee587040921
Nate Stemo <natestemo@hotmail.com>
2023-05-22 12:49:38 -0500

    removed slice from store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,8 @@
 import {configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../redux/counterSlice'
 import lifterReducer from '../redux/lifterSlice'
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         lifter: lifterReducer,
     },
 })
