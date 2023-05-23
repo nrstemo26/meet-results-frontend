c8c282e1b6dd22583a16149bc3a018e48d15ae99
Nate Stemo <natestemo@hotmail.com>
2023-05-23 09:37:01 -0500

    Link clicks take user to lifter dashboard

--- frontend/src/components/SearchBars/UserList.jsx
+++ frontend/src/components/SearchBars/UserList.jsx
@@ -1,10 +1,11 @@
-
+import {Link } from 'react-router-dom'
 
 const UserList = ({ users }) => {
 
   const nameToQueryString = (str) =>{
     console.log(str)
     let queryString = str.split(/\s+/).join('%20')
+    console.log('bout to return ',queryString)
     return queryString
   }
 
@@ -23,7 +24,9 @@ const UserList = ({ users }) => {
     for( let i = 0; i<amount; i++){
       if(users[i]){
       let user = users[i];
-      lis.push(<div className='border-primary-500 p-2 border-2 m-2 hover:text-white hover:bg-primary-500 hover:cursor-pointer' key={user} onClick={handleClick}>{user}</div> )
+      //can i delay doing the name->query string function??
+      lis.push(<Link to={`/api/v1/lifter/${nameToQueryString(user)}`} ><div className='border-primary-500 p-2 border-2 m-2 hover:text-white hover:bg-primary-500 hover:cursor-pointer' key={user} >{user}</div></Link> )
+      // lis.push(<Link to={`/api/v1/lifter/${nameToQueryString(user)}`} ><div className='border-primary-500 p-2 border-2 m-2 hover:text-white hover:bg-primary-500 hover:cursor-pointer' key={user} onClick={handleClick}>{user}</div></Link> )
       }
     }
     return lis;
