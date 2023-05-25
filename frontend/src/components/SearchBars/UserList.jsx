9e8945f8237435f06eca82713e11674fa96b6da9
Nate Stemo <natestemo@hotmail.com>
2023-05-24 19:34:40 -0500

    user list outputs click event if its a session

--- frontend/src/components/SearchBars/UserList.jsx
+++ frontend/src/components/SearchBars/UserList.jsx
@@ -1,7 +1,7 @@
 import {Link } from 'react-router-dom'
 
-const UserList = ({ users }) => {
-
+const UserList = ({ users ,isSession, setSessionAthletes}) => {
+  
   const nameToQueryString = (str) =>{
     // console.log(str)
     let queryString = str.split(/\s+/).join('%20')
@@ -9,23 +9,21 @@ const UserList = ({ users }) => {
     return queryString
   }
 
+   const handleClick = (e) =>{
+    setSessionAthletes(arr=>[...arr,e.target.textContent])
+   }
 
-  const handleClick = (e) =>{
-    console.log('clicked')
-    //I dont need the ? but maybe I do
-    console.log('?' + nameToQueryString(e.target.textContent))
-
-    //function moveToLifterDashboard(takes in querystring?)
-    //need name
-  }
-
-  let lis = []
   const renderUsers = (amount) => {
+    let lis = []
     for( let i = 0; i<amount; i++){
       if(users[i]){
       let user = users[i];
       //can i delay doing the name->query string function??
-      lis.push(<Link to={`/api/v1/athlete/${nameToQueryString(user)}`} ><div className='border-primary-500 p-2 border-2 m-2 hover:text-white hover:bg-primary-500 hover:cursor-pointer' key={user} >{user}</div></Link> )
+      if(isSession){
+        lis.push(<div className='border-primary-500 p-2 border-2 m-2 hover:text-white hover:bg-primary-500 hover:cursor-pointer' key={user}  onClick={(e)=> handleClick(e)}>{user}</div> )  
+      }else{
+        lis.push(<Link to={`/api/v1/athlete/${nameToQueryString(user)}`} ><div className='border-primary-500 p-2 border-2 m-2 hover:text-white hover:bg-primary-500 hover:cursor-pointer' key={user} >{user}</div></Link> )
+      }
       // lis.push(<Link to={`/api/v1/lifter/${nameToQueryString(user)}`} ><div className='border-primary-500 p-2 border-2 m-2 hover:text-white hover:bg-primary-500 hover:cursor-pointer' key={user} onClick={handleClick}>{user}</div></Link> )
       }
     }
