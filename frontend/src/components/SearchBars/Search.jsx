2bbedb3dcd69947ea80a672cdc9b43b69a269938
Nate Stemo <natestemo@hotmail.com>
2023-05-24 19:33:43 -0500

    session props added to user list

--- frontend/src/components/SearchBars/Search.jsx
+++ frontend/src/components/SearchBars/Search.jsx
@@ -5,97 +5,9 @@ import {useDispatch} from 'react-redux'
 import { getAllAthletes } from "../../redux/lifterSlice";
 
 
-const lifterData = [
-    // "athletes": [
-        "Nicholas  Allevato",
-        "Mikerline  Apollon",
-        "Luke  Carroll",
-        "Lorelei  Dalton",
-        "Dario  Garcia",
-        "Madison  Gray",
-        "Theresa  Hadley",
-        "Kristin  Leet",
-        "Makiyah  Nickerson",
-        "Diane  Ortiz",
-        "Emily  Roberts",
-        "Jordan  Thomas",
-        "Cheyenne  Warmack",
-        "Leanne  Watson",
-        "Katelyn  Witte",
-        "John (Brad) King",
-        "James (Jim) Storch",
-        "Katherine (Katie) Plante",
-        "Charles (Nick) Gordon",
-        "Yehwalashet (Nyah) Matson",
-        "Benjamin Abbott",
-        "Kelsey Abdallah",
-        "Nadeem Abdin",
-        "Victoria Abell",
-        "Shanna Aberson",
-        "Keenan Abeyta",
-        "Hunter Abila",
-        "Christine Abrams",
-        "Alexander Aceituno",
-        "Samantha Acevedo",
-        "Carlee Acevedo-Fuller",
-        "Aaron Acosta",
-        "Carlos Acosta",
-        "Jason Acosta",
-        "Javier Acosta",
-        "Jordan Acosta",
-        "Matthew Adamcheck",
-        "Robert Adamire",
-        "Haley Adams",
-        "Dandre Adams",
-        "McKenna Adams",
-        "Marcus Adams",
-        "Jamie Adams",
-        "Cynthia Adams",
-        "Brent Adams",
-        "Chad Addison",
-        "Jordan Adel",
-        "David Adeyemo",
-        "Courtney Adkins",
-        "Brittney Adkisson",
-        "Zachary Adler",
-        "James Adrian",
-        "Robert Adriano",
-        "James Aftosmis",
-        "Marie Agbah Hughes",
-        "Karen Agena",
-        "Manuel Aguero",
-        "Carla Aguilar",
-        "Billy Aguilar",
-        "Patricia Aguilar",
-        "Casey Aguilar-Gervase",
-        "John Aguinaldo",
-        "Emily Aguirre",
-        "Nabeela Ahmed",
-        "Andre Ainsworth",
-        "Inna Aizenshtein",
-        "Nu Akalegbere",
-        "Jonathan Akin",
-        "Jacob Alamina",
-        "B. Alanna C. Fisk",
-        "Alex Alas",
-        "Miguel Alava",
-        "Dana Albanese",
-        "Timothy Albergotti",
-        "Mark Albers",
-        "Jaclyn Alberts",
-        "Dimitri Albury",
-        "Christopher Alcantara",
-        "Jairo Alcaraz",
-        "Mark Alcaraz",
-        "Juan Alcazar",
-        "Kim Alderwick",
-        "Elizabeth Aldrich"]
-    // }
-
-const Search = () => {
+const Search = ({isSession, setSessionAthletes}) => {
   const dispatch = useDispatch()
-
-  const [users, setUsers] = useState(lifterData);
+  const [users, setUsers] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
@@ -118,7 +30,7 @@ const Search = () => {
     return (
         <div className=" text-center">
             <SearchBar onSearch={handleSearch} />
-            <UserList users={filteredUsers} />
+            <UserList users={filteredUsers} isSession={isSession} setSessionAthletes={setSessionAthletes} />
         </div>
     )
 }
