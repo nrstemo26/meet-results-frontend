f924986af72ef311030facb3b530694acb245833
Nate Stemo <natestemo@hotmail.com>
2023-05-27 18:37:10 -0500

    toggles as meets results are shown

--- frontend/src/components/Session/SessionAthlete.jsx
+++ frontend/src/components/Session/SessionAthlete.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useDispatch } from "react-redux";
 import {getAthlete} from "../../redux/lifterSlice"
-import BestLifts from '../Dashboards/LifterDashboard/BestLits';
-import {FiArrowDown} from 'react-icons/fi'
+import {FiArrowDown, FiArrowUp} from 'react-icons/fi'
 import { organizeByDate, renderSessionResults } from '../../utilities/date_utils';
 
 function SessionAthlete({name }){
@@ -18,34 +17,11 @@ function SessionAthlete({name }){
         console.log(data)
         setMeetHistory(meet_history)
         setStats(stats)
-      // console.log('stats',stats)
-      // console.log('history',meet_history)
       }
       getUserData()
-    },[dispatch])
+    },[dispatch, name])
 
-    const renderResults = (arr) => {
-        return arr.map((meet,index)=>{
-            if(index > 4){
-                return 
-            }else{
-            let meetData = meetHistory[meet]
-            let [day, month, year] = meetData["Date"].split(' ').slice(1,4)
-            return (
-            <div className="flex justify-evenly border-2 border-primary-400 p-4 my-2" key={meet}>    
-                <div>
-                    <div>{month}/{day}/{year}</div>
-                    <div>Best Lifts:{meetData["Best Snatch"]}/{meetData["Best C&J"]}/{meetData["Total"]}</div>
-                </div>
-                <div>
-                    <div>Sn: {meetData["Snatch Lift 1"]}/{meetData["Snatch Lift 2"]}/{meetData["Snatch Lift 3"]}</div>
-                    <div>CJ: {meetData["C&J Lift 1"]}/{meetData["C&J Lift 2"]}/{meetData["C&J Lift 3"]}</div>
-                </div>
-            </div>
-            )
-            }
-        })
-    }
+    
     const toggleMeetHistory = ()=>{
         setAreMeetsVisible((curr)=>!curr)
     }
@@ -57,8 +33,8 @@ function SessionAthlete({name }){
             : 'loading stats'}
 
             <div className='flex justify-between' onClick={()=>toggleMeetHistory()}>
-                <div>last 5 meets</div>
-                <FiArrowDown/>
+                {areMeetsVisible ? 
+                (<><div>hide last 5 meets</div><FiArrowUp/></>) : (<><div>show last 5 meets</div><FiArrowDown/></>)}
             </div>
             {areMeetsVisible && meetHistory ? renderSessionResults(organizeByDate(meetHistory), meetHistory) :''}
         </div>
