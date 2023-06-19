89941edacd3e1220d139892246780746e8ffe880
Nate Stemo <natestemo@hotmail.com>
2023-06-19 11:57:56 -0500

    all totals reads from redux store instead of receiving a prop

--- frontend/src/components/Dashboards/Athlete/AllTotals.jsx
+++ frontend/src/components/Dashboards/Athlete/AllTotals.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react"
 import { organizeByNewestDate, renderTotalResults } from "../../../lib/date_utils"
+import { useSelector } from "react-redux"
 
 
-function AllTotals({ meetHistory }){
+function AllTotals(){
+    const { data:{meet_history} } = useSelector( (state)=> state.athlete )|| 'blah';
+   
 
     return(
         <div className="bg-secondary-500 p-6 rounded-lg flex flex-col flex-auto overflow-hidden shadow-lg">          
-            {meetHistory? 
-            renderTotalResults(organizeByNewestDate(meetHistory), meetHistory)   
+            {meet_history? 
+            renderTotalResults(organizeByNewestDate(meet_history), meet_history)   
             :
             <h1>no meet history</h1>}
-        </div>
-           
+        </div>   
     )
-    
 }
 
 export default AllTotals
