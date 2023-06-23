6ce3b816a1f041577e05929a3531ee9879cbe416
Nate Stemo <natestemo@hotmail.com>
2023-06-23 09:33:48 -0500

    slice sends data to paginate get all req with search filter, page num, and page size

--- frontend/src/features/athleteSlice.js
+++ frontend/src/features/athleteSlice.js
@@ -33,13 +33,20 @@ export const getAthlete = createAsyncThunk(
 
 export const getAllAthletes = createAsyncThunk(
   'athletes',
-  async(thunkAPI)=>{
+  async(data, thunkAPI)=>{
     try{
       //thunkapi has a function that 
       //can get any chunk of the state
       //thunkAPI.getState().sliceofstate.propname.propname
-
-      const response = await axios.get(baseUrl + 'athletes')
+     
+      const config = {
+        params:{
+          name: data.name ,
+          page: data.page ,
+          pageSize: data.pageSize ,
+        }
+      }
+      const response = await axios.get(baseUrl + 'athletes', config)
       return response.data;
     }catch(error){
       console.log(error)
@@ -49,7 +56,7 @@ export const getAllAthletes = createAsyncThunk(
 
 export const getTrendingAthletes = createAsyncThunk(
   'trending_athletes',
-  async()=>{
+  async() => {
     try{
       const response = await axios.get(baseUrl + 'trending_athletes')
       return response.data;
@@ -68,6 +75,8 @@ export const athleteSlice = createSlice({
         builder
           .addCase(getAthlete.pending, (state) => {
             state.isLoading = true;
+            // state.isError = false;
+            // state.isSuccess = false
           })
           .addCase(getAthlete.fulfilled, (state, action) => {
             state.isLoading = false;
@@ -79,7 +88,39 @@ export const athleteSlice = createSlice({
             state.isLoading = false;
             state.isError = true;
             state.error = action.error.message;
-          });
+          })
+          // .addCase(getAllAthletes.pending, (state) => {
+          //   state.isLoading = true;
+          //   state.isError = false;
+          //   state.isSuccess = false
+          // })
+          // .addCase(getAllAthletes.fulfilled, (state, action) => {
+          //   state.isLoading = false;
+          //   state.isSuccess = true;
+          //   state.isError = false;
+          //   state.data = action.payload;
+          // })
+          // .addCase(getAllAthletes.rejected, (state, action) => {
+          //   state.isLoading = false;
+          //   state.isError = true;
+          //   state.error = action.error.message;
+          // })
+          // .addCase(getTrendingAthletes.pending, (state) => {
+          //   state.isLoading = true;
+          //   state.isError = false;
+          //   state.isSuccess = false
+          // })
+          // .addCase(getTrendingAthletes.fulfilled, (state, action) => {
+          //   state.isLoading = false;
+          //   state.isSuccess = true;
+          //   state.isError = false;
+          //   state.data = action.payload;
+          // })
+          // .addCase(getTrendingAthletes.rejected, (state, action) => {
+          //   state.isLoading = false;
+          //   state.isError = true;
+          //   state.error = action.error.message;
+          // })
       },
 })
 
