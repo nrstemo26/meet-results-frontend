8b9900edb66c67707fe1f2ce64464e24ae08c45d
Nate Stemo <natestemo@hotmail.com>
2023-06-27 08:25:18 -0500

    comments for makeToast function

--- frontend/src/lib/toast/toast_utils.js
+++ frontend/src/lib/toast/toast_utils.js
@@ -10,8 +10,21 @@
 //     }, 5000); 
 // }
 
+
 ///curried version
-export const makeToast = (setShowToast, setToastType, setToastMessage)=> {
+//SAMPLE USAGE
+//we call this function twice
+
+// first time we set all of the toast state
+// const makeToast = makeToast_(setShowToast, setToastType, setToastMessage)
+// the underscore is there to show its a curried function and we have to rename it
+// this binds all future makeToast calls to the state in our component
+
+//now we just call this to make a toast by supplying a message 
+//and an optional toast type... just set type to false otherwise
+// makeToast('youve successfully made an account','success')
+
+export const makeToast_ = (setShowToast, setToastType, setToastMessage)=> {
     return (msg, type) => {    
         setToastMessage(msg);
         if(type) setToastType(type)
