755aea39c2d97aa14255d446787c9dc9eff8a9e2
murph <37813681+jmdevane@users.noreply.github.com>
2023-06-29 16:14:55 +0000

    trending styling

--- frontend/src/components/SearchBars/Trending.jsx
+++ frontend/src/components/SearchBars/Trending.jsx
@@ -23,6 +23,7 @@ function Trending({ isSession }){
     return(
         <div className="shadow-lg m-2 p-4">
             <h1 className="text-lg text-primary-950 font-bold text-center">Trending Athletes 🔥📈</h1>
+            <p className="text-sm text-center font-bold text-gray-700 border-b border-primary-100">Last 24 Hours</p>
             <UserList users={athletes} isSession={isSession}></UserList>
         </div>
     )
