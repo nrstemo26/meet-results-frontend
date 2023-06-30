4d804c0a72780a0d3c3d5435b2e00bf137f2e902
murph <37813681+jmdevane@users.noreply.github.com>
2023-06-30 12:13:07 +0000

    home page copy

--- frontend/src/components/LoadingWrapperSandbox.jsx
+++ frontend/src/components/LoadingWrapperSandbox.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import { Link } from 'react-router-dom';
 import { Spinner } from "../pages/Spinner"
 import FeatureRequest from "./Widgets/FeatureRequest"
 import Trending from './SearchBars/Trending'
@@ -15,10 +16,39 @@ function withLoading(Component){
 const MyComponent = ({ data })=>{
     return (
         <div className="sm:w-2/3 bg-gradient-to-r from-transparent via-cyan-50 to-transparent h-screen">
-            {/* <h1>HOME</h1> */}
             <h2 className="text-primary-950 font-bold text-5xl sm:w-1/2 p-2 pt-16 ml-8">{data.title}</h2>
             <h2 className="text-primary-950 font-bold text-3xl sm:w-1/2 p-2 mt-4 mx-8 leading-relaxed">{data.subtitle}</h2>
-            <p className="mt-8 ml-8 sm:w-1/3 p-2 text-primary-950 leading-loose">{data.description}</p>
+            <p className="mt-8 ml-8 sm:w-1/3 p-2 text-primary-950 leading-loose font-semibold">{data.description}</p>
+            <ul className="list-none m-8 p-2 text-gray-700 text-sm md:text-m space-y-2">
+                <li>
+                    <span className="text-xl">🏋️‍♂️ </span>
+                    <span className="font-semibold">Visualize. </span>
+                    <Link to={`/api/v1/meets/`} className="hover:text-primary-400">
+                        Competition results database covering x,000+ athletes & 4,000+ local and national events.
+                    </Link>
+                </li>
+                <li>
+                    <span className="text-xl">🧐 </span>
+                    <span className="font-semibold">Analyze. </span>
+                    <Link to={`/api/v1/athletes/`} className="hover:text-primary-400">
+                        Tap into historical trends and advanced stats across 10+ years of American weightlifting.
+                    </Link>
+                </li>
+                <li>
+                    <span className="text-xl">🏗️ </span>
+                    <span className="font-semibold">Build. </span>
+                    <Link to={`/api/v1/session/`} className="hover:text-primary-400">
+                        Create a watchlist to track your favorite weightlifters (or competition).
+                    </Link>
+                </li>
+                <li>
+                    <span className="text-xl">🥇 </span>
+                    <span className="font-semibold">Elevate. </span>
+                    <Link to={`/api/v1/insights/`} className="hover:text-primary-400">
+                        Dial in your competition coaching with unique and on-demand insights.
+                    </Link>
+                </li>
+            </ul>
         </div>
     )
 }
@@ -46,7 +76,7 @@ const HomeComponent = ()=>{
   return(
     <div className='sm:flex w-100'>
         <WrappedComponent isLoading={isLoading} data={data}/>
-        <div className="md:fixed md:right-0 md:m-8 md:w-1/4">
+        <div className="md:fixed md:right-0 mt-8 md:m-8 md:w-1/4">
             <Trending />
         </div>
         <FeatureRequest />
