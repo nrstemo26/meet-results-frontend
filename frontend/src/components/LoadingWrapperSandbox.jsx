be49557138fb733f642f241c86c95336bb65cc6a
murph <37813681+jmdevane@users.noreply.github.com>
2023-06-30 12:17:39 +0000

    home page styling and copy

--- frontend/src/components/LoadingWrapperSandbox.jsx
+++ frontend/src/components/LoadingWrapperSandbox.jsx
@@ -15,9 +15,9 @@ function withLoading(Component){
 
 const MyComponent = ({ data })=>{
     return (
-        <div className="sm:w-2/3 bg-gradient-to-r from-transparent via-cyan-50 to-transparent h-screen">
-            <h2 className="text-primary-950 font-bold text-5xl sm:w-1/2 p-2 pt-16 ml-8">{data.title}</h2>
-            <h2 className="text-primary-950 font-bold text-3xl sm:w-1/2 p-2 mt-4 mx-8 leading-relaxed">{data.subtitle}</h2>
+        <div className="sm:w-2/3 bg-gradient-to-r from-transparent via-cyan-50 to-transparent">
+            <h2 className="text-primary-950 font-bold text-5xl sm:w-2/3 p-2 pt-16 ml-8">{data.title}</h2>
+            <h2 className="text-primary-950 font-bold text-3xl sm:w-2/3 p-2 mt-4 mx-8 leading-relaxed">{data.subtitle}</h2>
             <p className="mt-8 ml-8 sm:w-1/3 p-2 text-primary-950 leading-loose font-semibold">{data.description}</p>
             <ul className="list-none m-8 p-2 text-gray-700 text-sm md:text-m space-y-2">
                 <li>
@@ -62,9 +62,9 @@ const HomeComponent = ()=>{
     
     const [isLoading, setIsLoading] = useState(true)
     const data = {
-        title: 'Lift Oracle.',
+        title: 'Consult the Lift Oracle.',
         subtitle: 'Weightlifting statistics & intelligence.',
-        description: "Powered by accomplished olympic weightlifting coaches and massive data nerds, Lift Oracle is your new destination for historical competition results and statistics."
+        description: "Powered by accomplished Olympic weightlifting coaches and massive data nerds, Lift Oracle is your new destination for historical competition results and statistics."
     }
 
     useEffect(()=>{
