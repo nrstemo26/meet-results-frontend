986ce3dacdba89f9c12321a1515209828990024a
Nate Stemo <natestemo@hotmail.com>
2023-07-19 10:34:15 -0500

    uses verify from slice

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import { useEffect } from 'react'
-import axios from 'axios';
 
 import { NotFound } from '../pages/NotFound'
 import WatchList from './WatchList/WatchList'
@@ -19,54 +18,26 @@ import { Home } from '../pages/Home/Home'
 import { Insights } from '../pages/Insights'
 import { About } from '../pages/About'
 
-//insert working url
-const baseUrl = 'http://192.168.0.108:5000'
+import { useDispatch } from 'react-redux';
+import { verify,reset } from '../features/authSlice';
 
 
-function Main({ isLoggedIn, setIsLoggedIn }) {
+function Main() {
+  const dispatch = useDispatch()
 
-    const updateLoggedInStatus = (status) => {
-      setIsLoggedIn(status);
-      console.log('Login status updated:', status);
-      // console.log(isLoggedIn);
-    };
+  useEffect(() => {
+    dispatch(verify())
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-          // Token exists in localStorage, authenticate with the backend
-          axios
-            .post(`${baseUrl}/user/verify-token`, { token }) // Replace with your backend endpoint for token verification
-            .then(response => {
-              const { valid } = response.data; // Assuming the backend returns a validity flag
-      
-              if (valid) {
-                // Token is valid, consider the user as logged in
-                setIsLoggedIn(true);
-              } else {
-                // Token is invalid, remove from localStorage and log the user out
-                localStorage.removeItem('token');
-                setIsLoggedIn(false);
-              }
-            })
-            .catch(error => {
-              console.log('Token verification failed:', error);
-              setIsLoggedIn(false);
-            });
-        }
-      }, []); // Run this effect only once on component mount
-    
-      useEffect(() => {
-        console.log('isLoggedIn:', isLoggedIn);
-      }, [isLoggedIn]);
-
+    //resets loading/ error/ etc states
+    dispatch(reset())
+  }, []); // Run this effect only once on component mount
 
 return (
     <div >
       <Routes>
           <Route path="*" element={<NotFound/>}/>
           {/* this route will need to change...needs an id? */}
-          <Route path="/api/v1/session" element={<WatchList isLoggedIn={isLoggedIn}/>} />
+          <Route path="/api/v1/session" element={<WatchList />} />
           <Route path="/api/v1/watchlist" element={<AccountDashboard/>} />
           <Route path="/api/v1/athletes" element={<Search/>}/>
           <Route path="/api/v1/athlete/:id" element={<AthleteDashboard/>}/>
@@ -80,9 +51,9 @@ return (
           <Route path="/about" element={<About/>}/>
           <Route path="/reset-request" element={<ResetRequest/>}/>
           <Route path="/reset-password" element={<PasswordReset/>}/>
-          <Route path="/login" element={<Login updateLoggedInStatus={updateLoggedInStatus} />} /> {/* Pass updateLoggedInStatus prop to Login */}
-          <Route path="/register" element={<Register updateLoggedInStatus={updateLoggedInStatus} />} /> {/* Pass updateLoggedInStatus prop to Register */}
-          <Route path='/account' element={<Account isLoggedIn={isLoggedIn} />}/>
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/register" element={<Register />} /> 
+          <Route path='/account' element={<Account />}/>
           <Route path="/confirmation" element={<Confirmation />} />
         </Routes>
 
