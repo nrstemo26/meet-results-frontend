e4cc6a604e8c0780a93f2f6f2a8c812e4c4028c1
Nate Stemo <natestemo@hotmail.com>
2023-07-24 08:50:39 -0500

    removed toast from import as we just use container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 import { useState } from 'react'
 import { BrowserRouter as Router} from 'react-router-dom'
-import { ToastContainer, toast} from 'react-toastify'
+import { ToastContainer} from 'react-toastify'
 
 import Main from './components/Main'
 import Navbar from './components/Navbar/Navbar'
