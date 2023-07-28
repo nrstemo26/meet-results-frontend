c42730df0b293529da15a28a142a0897cf3f5f4b
murph <37813681+jmdevane@users.noreply.github.com>
2023-07-28 00:14:47 +0000

    athlete export

--- frontend/src/components/Dashboards/Athlete/WatchlistBtn.jsx
+++ frontend/src/components/Dashboards/Athlete/WatchlistBtn.jsx
@@ -5,24 +5,72 @@ import { useSelector, useDispatch } from 'react-redux'
 import { removeFromWatchlist,addToWatchlist } from '../../../features/watchlistSlice';
 import { BsFillClipboardCheckFill } from 'react-icons/bs'
 import {AiFillPlusCircle as AddCircle} from 'react-icons/ai'
-
+import { TiDownload } from 'react-icons/ti'
 import { useViewport } from "../../../hooks/useViewport";
+import { baseUrl } from '../../../config';
 
 
-const WatchlistBtn = ({toggleWatchlist, inWatchlist}) =>{
+const WatchlistBtn = ({toggleWatchlist, inWatchlist, name}) =>{
     const { width } = useViewport();
+    const user = useSelector((state) => state.auth.user)
+
+    const getFilenameFromContentDisposition = (contentDisposition) => {
+        const filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+        const matches = filenameRegex.exec(contentDisposition);
+        if (matches != null && matches[1]) {
+          return matches[1].replace(/['"]/g, '');
+        }
+        return 'watchlist.xlsx'; // Default filename if unable to extract from header
+      };
+      
+    const handleExport = () => {
+        
+        if (user) {
+          const token = localStorage.getItem('token');
+        //   const token = localStorage.getItem('user');
+          const credentials = btoa(`${token}:unused`);
+    
+          const xhr = new XMLHttpRequest();
+          xhr.open('POST', `${baseUrl}/api/v1/export`);
+          xhr.setRequestHeader('Authorization', `Basic ${credentials}`);
+          xhr.setRequestHeader('Content-Type', 'application/json');
+          xhr.responseType = 'blob';
+      
+          xhr.onload = function () {
+            if (xhr.status === 200) {
+              const contentDisposition = xhr.getResponseHeader('Content-Disposition');
+              const filename = getFilenameFromContentDisposition(contentDisposition);
+              const blob = new Blob([xhr.response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+              saveAs(blob, filename);
+            } else {
+              console.error(xhr.statusText);
+              // Handle the error
+            }
+          };
+      
+          xhr.onerror = function () {
+            console.error('Request failed');
+            // Handle the error
+          };
+      
+          xhr.send(JSON.stringify({ athlete: name }));
+        }
+      };
     
     return (
     <>
         {/* {width > 540 ? */}
         <div>
         {inWatchlist?
-            <BsFillClipboardCheckFill className="fill-primary-400 inline cursor-pointer" title="click to remove from watchlist"onClick={toggleWatchlist} size='24px'/> 
+            <BsFillClipboardCheckFill className="text-primary-950 text-2xl hover:text-primary-400 inline cursor-pointer m-1 mb-2" title="click to remove from watchlist"onClick={toggleWatchlist} /> 
             : 
-            <AddCircle className='fill-primary-400 inline cursor-pointer' title="click to add to watchlist" onClick={toggleWatchlist} size='24px'/>
+            <AddCircle className='text-primary-950 text-2xl hover:text-primary-400 inline cursor-pointer m-1 pb-1 mb-2' title="click to add to watchlist" onClick={toggleWatchlist} />
         }   
         
-            <button onClick={toggleWatchlist} className={`btn ${inWatchlist? '':'btn-follow'} p-0 px-2 b-2`}>{inWatchlist? 'remove from watchlist': 'add to watchlist'}</button>
+            {/* <button onClick={toggleWatchlist} className={`btn ${inWatchlist? '':'btn-follow'} p-0 px-2 b-2`}>{inWatchlist? 'remove from watchlist': 'add to watchlist'}</button> */}
+            <button className='text-primary-950 text-2xl hover:text-primary-400 m-1' onClick={handleExport}>
+                  <TiDownload />
+            </button>
         </div>
         {/* :''
         // this button is stickied to the lower right of the screen
