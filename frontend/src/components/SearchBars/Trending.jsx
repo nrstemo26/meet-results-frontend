a445f325b89f1458655f43ff9cbd8b645d3608ce
Nate Stemo <natestemo@hotmail.com>
2023-07-31 12:42:28 -0500

    trending uses redux to manage trending athlete state

--- frontend/src/components/SearchBars/Trending.jsx
+++ frontend/src/components/SearchBars/Trending.jsx
@@ -1,22 +1,23 @@
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useEffect, useState } from "react";
+
 import { getTrendingAthletes } from "../../features/athleteSlice";
 import UserList from './UserList';
 import PropTypes from 'prop-types';
 
 
 function Trending({ isWatchlist }){
-    const [pageSize, setPageSize] = useState(20)
     const dispatch = useDispatch();
-    const [athletes, setAthletes] = useState([])
+    const [pageSize, setPageSize] = useState(20)
+    const trendingAthletes = useSelector((state)=> state.athlete.trendingAthletes)
     
 
     useEffect(()=>{
-        const fetchUsers = async()=>{
+        const fetchUsers = async () => {
             // const athletes = (await dispatch(getTrendingAthletes({pageSize}))).payload
-            //could need the .data depending on backend version
-            const athletes = (await dispatch(getTrendingAthletes({pageSize}))).payload.data
-            setAthletes(athletes)
+            // could need the .data depending on backend version
+            await dispatch(getTrendingAthletes({pageSize}))
+            
         }
         fetchUsers()
     },[dispatch,pageSize])
@@ -26,7 +27,7 @@ function Trending({ isWatchlist }){
         <div className="shadow-lg m-2 p-4">
             <h1 className="text-lg text-primary-950 font-bold text-center">Trending Athletes 🔥📈</h1>
             <p className="text-sm text-center font-bold text-gray-700 border-b border-primary-100">Last 24 Hours</p>
-            <UserList users={athletes} isWatchlist={isWatchlist}></UserList>
+            <UserList users={trendingAthletes} isWatchlist={isWatchlist}></UserList>
         </div>
     )
 }
