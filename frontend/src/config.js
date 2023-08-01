beda355a57617d5170fb5e418f6859dcf4e59177
murph <37813681+jmdevane@users.noreply.github.com>
2023-08-01 20:44:35 +0000

    prod backend baseurl (commented out)

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 //change this whenever restarting backend
 // export const baseUrl = 'http://192.168.1.165:5000' // nate local
 export const baseUrl = 'http://192.168.86.27:5000' // murph local
+// export const baseUrl = 'https://liftoracle.com/api' // production
