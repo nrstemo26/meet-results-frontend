763f244b69e50758fce8b4a0076ded3264457247
murph <37813681+jmdevane@users.noreply.github.com>
2023-09-05 15:46:41 +0000

    about feature updated for paywall

--- frontend/src/components/Dashboards/Athlete/Insights.jsx
+++ frontend/src/components/Dashboards/Athlete/Insights.jsx
@@ -1,15 +1,16 @@
 import { useSelector } from "react-redux"
+import PaywallOverlay from "../../Widgets/PaywallOverlay"
 
 function Insights (){
     const { data:{stats} } = useSelector((state) => state.athlete)
     const { data:{advanced_stats} } = useSelector((state) => state.athlete)
     
-    if(stats){
-
-        return(
-            (!stats? <div className="text-gray-700">No stats for athlete.</div>:
-        (        
-        <div className="bg-secondary-500 p-6 rounded-lg flex flex-col flex-auto overflow-hidden shadow-lg p-2">
+    return(
+      <PaywallOverlay>
+        { !stats ? (
+            <div className="text-gray-700">No stats for athlete.</div>
+        ) : (        
+          <div className="bg-secondary-500 p-6 rounded-lg flex flex-col flex-auto overflow-hidden shadow-lg p-2">
           <h1 className="text-center text-xl text-primary-950 font-bold m-2 border-b border-primary-100">Insights</h1>
           <div className="text-left flex flex-col gap-1 mb-2">
               <a className="text-primary-950 border-2 border-secondary rounded-lg">Weight Class(es): <span className="font-mono text-gray-700 text-l">{stats["Weight Class(es)"]}kg</span></a>
@@ -29,12 +30,10 @@ function Insights (){
               <a className="text-primary-950 border-2 border-secondary rounded-lg">Opener Make Rate: <span className="font-mono text-gray-700 text-l">{advanced_stats['C&J']['Opener Make %']}</span></a>
               <a className="text-primary-950 border-2 border-secondary rounded-lg">Total Avg Reach: <span className="font-mono text-gray-70 text-l">{advanced_stats['C&J']['Total Avg Reach (Kg)']}kg</span></a>
           </div>
-        </div>
-    ))
-    
-    
+          </div>
+        )}
+      </PaywallOverlay>
     )
-    }
 }
 
 export default Insights;
---
+++ frontend/src/components/Widgets/PaywallOverlay.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useSelector } from "react-redux";
+import { proLink } from "../../config"
+
+function PaywallOverlay({ children }) {
+  const isSubscribed = useSelector((state) => state.auth.isSubscribed)
+  const enhancedChildren = isSubscribed 
+    ? children 
+    : React.cloneElement(children, {
+        className: `${children.props.className || ''} blur-sm`,
+      });
+
+  return (
+    <div className="relative">
+      {enhancedChildren}
+      {!isSubscribed && (
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <div className="
+          absolute top-0 left-0 right-0 bottom-0
+          bg-gradient-to-r from-white via-primary-50 to-white
+          opacity-50
+          flex items-center justify-center z-0
+          "></div>
+          <a href={proLink} className="z-20">
+            <button className="bg-primary-950 hover:bg-primary-400 text-white font-bold py-2 px-4 rounded">
+              Unlock with Lift Oracle Pro
+            </button>
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PaywallOverlay;
