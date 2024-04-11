1e6dd1a6ad65bb1e043a2dc4272a9084b92ab717
murph <37813681+jmdevane@users.noreply.github.com>
2024-04-11 19:42:08 +0000

    timer on startlist paywall

--- frontend/src/components/Dashboards/Meet/Upcoming.jsx
+++ frontend/src/components/Dashboards/Meet/Upcoming.jsx
@@ -4,72 +4,62 @@ import { updateMetaTags } from '../../../lib/seo_utils';
 import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getUpcomingMeet } from '../../../features/meetSlice'
+import PaywallOverlay from '../../Widgets/PaywallOverlay'
 import AthleteDashboard from './StartList'
 
 
 
 const UpcomingMeetDashboard = () => {
-  const [requestSent, setRequestSent] = useState(false)
+  const [requestSent, setRequestSent] = useState(false);
+  const [paywallActive, setPaywallActive] = useState(false);
   const dispatch = useDispatch();
-  const {data, isLoading, isError, isSuccess, message} = useSelector( (state) => state.meet )
+  const { data, isLoading, isError } = useSelector((state) => state.meet);
 
-  const pageTitle = data ? `${data['metadata']['meet_name']} - Lift Oracle`: 'Lift Oracle';
-  const descriptionContent = data ? `Olympic weightlifting meet startlist - ${data['metadata']['meet_name']}. Preview weight classes, entry totals, etc.`: 'Loading meet information';
-  
-  useEffect(()=>{
-    if(isError){
-      console.log('there is an error')
-    }
-    const getMeetData = async()=>{
-      const urlArray = window.location.pathname.split('/')
-      const meetName = urlArray[urlArray.length - 1]
-      if(meetName != 'meets'){
-        console.log('getting meet')
-        dispatch(getUpcomingMeet(meetName))
+  useEffect(() => {
+    if (!requestSent) {
+      const urlArray = window.location.pathname.split('/');
+      const meetName = urlArray[urlArray.length - 1];
+      if (meetName !== 'meets') {
+        dispatch(getUpcomingMeet(meetName));
+        setRequestSent(true);
       }
-
     }
-    if(!requestSent){
-      getMeetData()
-      setRequestSent(true)
 
-    }
-  },[dispatch, isError, isSuccess, message, requestSent])
-  
-  if(isLoading){
-    return <Spinner/>
-  }
-  if(isError){
-    return <Error/>
-  }
+    // Paywall timer
+    const timer = setTimeout(() => {
+      setPaywallActive(true);
+    }, 30000); // Activate paywall after 30 seconds
 
-    return (
-      
-      <div className='dashboard-container'>
-        {updateMetaTags(pageTitle, descriptionContent)}
-          <div className='bg-secondary-500 p-5 rounded-xl'>
-            <h1 className="text-center text-primary-950 text-2xl font-bold m-2">{data ? data['metadata']['meet_name'] : 'loading'}</h1>
-            {/* {
-              data?
-              (<ChartWrapper/>):
-              'nothing'
-            } */}
-          </div>
+    return () => clearTimeout(timer);
+  }, [dispatch, requestSent]);
 
-        
-        <div className="my-2 flex flex-wrap gap-2 justify-evenly">
-          { data ?
-          (
-            <>
-              <AthleteDashboard meetData={data}/>
-            </>
-          ):
-          'no data'
-          }
-        </div>
+  if (isLoading) return <Spinner />;
+  if (isError) return <Error />;
+
+  const meetContent = (
+    <div className="my-2 flex flex-wrap gap-2 justify-evenly">
+      {data ? <AthleteDashboard meetData={data} /> : 'No data available'}
+    </div>
+  );
+
+  return (
+    <div className='dashboard-container'>
+      {updateMetaTags(data ? `${data['metadata']['meet_name']} - Lift Oracle` : 'Lift Oracle', 
+                      data ? `Olympic weightlifting meet startlist - ${data['metadata']['meet_name']}. Preview weight classes, entry totals, etc.` : 'Loading meet information')}
+      <div className='bg-secondary-500 p-5 rounded-xl'>
+        <h1 className="text-center text-primary-950 text-2xl font-bold m-2">{data ? data['metadata']['meet_name'] : 'Loading...'}</h1>
       </div>
-    )
-  }
-  
-  export default UpcomingMeetDashboard;
+      
+      {paywallActive ? (
+        <PaywallOverlay buttonText="Unlock Full Access with Lift Oracle Pro">
+          {meetContent}
+        </PaywallOverlay>
+      ) : (
+        meetContent
+      )}
+    </div>
+  );
+};
+
+export default UpcomingMeetDashboard;
   
